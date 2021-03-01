12716c407167096129f7a0a698d6b1459297202c
conejogreens <71003312+conejogreens@users.noreply.github.com>
2021-03-01 02:56:00 -0800

    Update email.js

--- assets/js/email.js
+++ assets/js/email.js
@@ -7,5 +7,5 @@ function defer(method) {
 }
 
 defer(function () {
-  $('#email').html("\u003Cn uers=\"znvygb:wbva@pbarwbterraf.bet\" ery=\"absbyybj\"\u003Ewbva@pbarwbterraf.bet\u003C/n\u003E".replace(/[a-zA-Z]/g,function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);}));
+  $('#email').html("\u003Cn uers=\"znvygb:uryyb@pbarwbterraf.bet\" ery=\"absbyybj\"\u003Euryyb@pbarwbterraf.bet\u003C/n\u003E".replace(/[a-zA-Z]/g,function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);}));
 });
